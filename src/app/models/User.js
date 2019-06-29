ca2f87a66fbe363ae3fb154a66da1f5279e832c5
= <av.vessoni@gmail.com>
2019-06-29 15:28:43 -0300

    Remove import

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,6 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
 import bcrypt from 'bcryptjs';
-import { async } from 'rxjs/internal/scheduler/async';
 
 class User extends Model {
   static init(sequelize) {
