f9ed134bfc3f4646dcfa37170562424127359086
Alexandre Almeida Vessoni <av.vessoni@gmail.com>
2019-08-15 15:29:47 -0300

    add env, and other exceptions to treat

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '962c1f3ccdc456',
-    pass: '4a78eb65d6effa',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
